2e35872a54077eb7053bba403b4d5e3c84e38310
CunMayday <caltinoz@purdueglobal.edu>
2025-11-17 00:28:44 -0500

    lightened react version

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,4 @@
 import { Switch, Route, Router as WouterRouter } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/Home";
 import NotFound from "@/pages/not-found";
 
@@ -18,14 +14,7 @@ function Router() {
 }
 
 function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
+  return <Router />;
 }
 
 export default App;
